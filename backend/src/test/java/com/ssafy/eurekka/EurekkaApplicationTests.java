48f9690a360a686d32bd5745d111e5197c9560df
정수림 <smill5059@naver.com>
2021-04-26 14:06:34 +0900

    Add backend

---
+++ backend/src/test/java/com/ssafy/eurekka/EurekkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.eurekka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekkaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
