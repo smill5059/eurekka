3b70e67e249b71c01b8b8abd8bf73b3ea0eff79d
w00k0805 <w00k0805@naver.com>
2021-04-28 14:26:29 +0900

    Modify LoginScreen.tsx
    - add naver api login in frontend/src/screens/LoginScreens.tsx
    - modify page title in frontend/App.tsx
    - add 'scheme' : 'mycoolredirect' in frontend/app.json

--- frontend/src/screens/HomeScreen.tsx
+++ frontend/src/screens/HomeScreen.tsx
@@ -1,17 +1,17 @@
-import * as React from 'react';
+import React from 'react';
 import { Button, View, Text } from 'react-native';
 
 function HomeScreen({ navigation }) {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Details Screen</Text>
-        <Button
-            title="Go to Home... again"
-            onPress={() => navigation.push('Home')}
-        />
-        <Button title="Go back" onPress={() => navigation.goBack()} />
-        </View>
-    );
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to Home... again"
+        onPress={() => navigation.push('Home')}
+      />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
+  );
 }
 
 export default HomeScreen;
--- frontend/src/screens/LoginScreen.tsx
+++ frontend/src/screens/LoginScreen.tsx
@@ -1,16 +1,82 @@
-import * as React from 'react';
-import { Button, View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { Button, View, Text, StyleSheet } from 'react-native';
+import * as AuthSession from 'expo-auth-session';
+import axios from 'axios';
 
 function LoginScreen({ navigation }) {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-        <Button
-          title="Login"
-          onPress={() => navigation.navigate('Home')}
-        />
-      </View>
+  const [NV_APP_ID, setId] = useState<string>();
+  const [NV_APP_SECRET, setSecret] = useState<string>();
+  const [STATE_STRING, setState] = useState<string>();
+
+  const [token, setToken] = useState<string>();
+  const [code, setCode] = useState<string>();
+  const [user, setUser] = useState<Object>();
+
+  async function getNaverInfo() {
+    const Info: {
+      NV_APP_ID: string;
+      NV_APP_SECRET: string;
+      STATE_STRING: string;
+    } = await axios.get(`http://localhost:8080/user/naver/state`);
+
+    setId(Info.NV_APP_ID);
+    setSecret(Info.NV_APP_SECRET);
+    setState(Info.STATE_STRING);
+  }
+
+  async function handlePressAsync() {
+    getNaverInfo();
+
+    let redirectUrl = AuthSession.getRedirectUrl();
+    console.log(redirectUrl);
+    console.log(encodeURIComponent(redirectUrl));
+    const result = await AuthSession.startAsync({
+      authUrl: `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NV_APP_ID}&redirect_uri=${encodeURIComponent(
+        redirectUrl
+      )}&state=${STATE_STRING}`,
+    });
+    console.log('result', result);
+    if (result.type === 'success') setCode(result.params.code);
+    handleGetAccess();
+  }
+
+  async function handleGetAccess() {
+    const accessToken: { access_token } = await axios.get(
+      `https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id=${NV_APP_ID} &client_secret=${NV_APP_SECRET} &code=${code}&state=${STATE_STRING}`
     );
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${accessToken.access_token}`,
+      },
+    };
+
+    setToken(accessToken.access_token);
+
+    const { data } = await axios.get(
+      'https://openapi.naver.com/v1/nid/me',
+      config
+    );
+    console.log(data);
+    setUser(data);
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+
+  return (
+    <View style={styles.container}>
+      <Button title="goToHome" onPress={() => navigation.navigate('Home')} />
+      <Text>네이버 아이디로 시작하기</Text>
+      <Button title="Login" onPress={handlePressAsync} />
+    </View>
+  );
 }
- 
+
 export default LoginScreen;
