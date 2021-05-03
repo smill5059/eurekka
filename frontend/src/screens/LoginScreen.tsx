81a8dd39ca39ea56dc9a42da0cb23782cfbf4b24
w00k0805 <w00k0805@naver.com>
2021-05-04 00:18:08 +0900

    Modkfy Login Page
    - android/app/build.gradle : 안드로이드에서 gif 동작하도록 fresco
      dependency 추가
    - assets/ icons 폴더 삭제, images 폴더 생성 및 이미지 파일 추가와 이동
    - global.d.ts : '*.gif' module 추가
    - src/App.tsx : root 컴포넌트 정리, 로딩 컴포넌트 제거
    - src/components/ : 안쓰는 컴포넌트 정리
    - src/images.tsx : assets 안의 이미지파일 경로 한 곳에 모아두고 export
    - src/navigations/AuthStack.tsx : Login 컴포넌트에 header 숨김
    - src/screens/LoginScreen.tsx: 카카오에서 받은 인증토큰 서버로 전달,
      화면 구성 정리
    - src/theme.tsx : 앱에서 사용할 색상 추가
    
    
    Former-commit-id: 858475b0ad89d67c3bcc3426d5002a86cd3ae757

--- frontend/src/screens/LoginScreen.tsx
+++ frontend/src/screens/LoginScreen.tsx
@@ -1,36 +1,82 @@
-import React, { useState } from 'react';
-import styled from 'styled-components/native';
+import React from 'react';
 import { KakaoOAuthToken, login } from '@react-native-seoul/kakao-login';
-import { Text, Button } from 'react-native';
+import { View, Image, TouchableOpacity, StyleSheet } from 'react-native';
 import axios from 'axios';
+import { images } from '../images';
+import { theme } from '../theme';
 
+// kakao 로그인 실행하는 첫 화면
+// jwt토큰 발급 시 다음 화면으로 넘기는 부분 구현 예정
 const LoginScreen = ({ navigation }) => {
-  const Container = styled.View`
-    flex: 1;
-    justify-content: center;
-    align-items: center;
-    background-color: ${({ theme }) => theme.background};
-  `;
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      padding: 10,
+      backgroundColor: theme.background,
+    },
+    header: {
+      flex: 2,
+      alignItems: 'center',
+    },
+    logo: {
+      width: '80%',
+      height: '80%',
+      resizeMode: 'contain',
+      marginTop: 10,
+    },
+    body: {
+      flex: 5,
+      alignItems: 'center',
+    },
+    img: {
+      width: '100%',
+      height: '100%',
+      resizeMode: 'contain',
+      padding: 10,
+    },
+    footer: {
+      flex: 1,
+    },
+    btnImg: {
+      height: '100%',
+      width: '60%',
+      resizeMode: 'contain',
+      alignSelf: 'center',
+    },
+  });
 
-  const [result, setResult] = useState<string>('');
-
+  // kakao login 실행되면 받는 인증 토큰 서버로 전달
   const signInWithKakao = async (): Promise<void> => {
     const token: KakaoOAuthToken = await login();
 
-    setResult(JSON.stringify(token.accessToken));
-    // console.log(result);
-    const res = await axios.post(`http://localhost:8080/user/kakao/login`, {
-      token: result,
-    });
-    console.log(res);
+    axios
+      .post(`http://10.0.2.2:8080/user/kakao/login`, {
+        token: token.accessToken,
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
     // navigation.navigate('Home');
   };
 
+  // 시작화면 (로고, 냉장고, 로그인 버튼)
   return (
-    <Container>
-      <Text style={{ fontSize: 30 }}>Login Screen</Text>
-      <Button title="카카오 로그인" onPress={() => signInWithKakao()} />
-    </Container>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={images.loadImg} style={styles.logo} />
+      </View>
+      <View style={styles.body}>
+        <Image source={images.fridgeImg} style={styles.img} />
+      </View>
+      <View style={styles.footer}>
+        <TouchableOpacity onPressOut={() => signInWithKakao()}>
+          <Image source={images.btnImg} style={styles.btnImg} />
+        </TouchableOpacity>
+      </View>
+    </View>
   );
 };
 
