13cf739e9a91c6efe3f71e116fd1628f73657f79
정수림 <smill5059@naver.com>
2021-05-04 15:08:48 +0900

    Modify for request findAllProduct - backend/eurekka/vo/Product.java : category attribute 추가 - backend/eurekka/service/RefrigeratorServiceImpl.java  - createProduct, updateAbandon, updateEat에서 category 파라미터 삭제  - findAllProduct 반환값 List<Product>로 변경  - ProductComparator 추가
    Former-commit-id: bb669fd8e8304747e7c17d025560f82d53670c97

--- backend/src/main/java/com/ssafy/eurekka/service/RefrigeratorService.java
+++ backend/src/main/java/com/ssafy/eurekka/service/RefrigeratorService.java
@@ -7,9 +7,9 @@ import org.bson.types.ObjectId;
 
 public interface RefrigeratorService {
 
-  Refrigerator createProduct(ObjectId id, int category, Product product);
-  List[] findAllProduct(ObjectId id);
+  Refrigerator createProduct(ObjectId id, Product product);
+  List<Product> findAllProduct(ObjectId id);
   List<Product> findByCategory(ObjectId id, int category);
-  void updateAbandon(String email, int category, Product product);
-  void updateEat(String email, int category, Product product);
+  void updateAbandon(String email, Product product);
+  void updateEat(String email, Product product);
 }
