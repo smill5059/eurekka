027116c839a27692da9f53aa264161c3d1acee8f
w00k0805 <w00k0805@naver.com>
2021-05-12 23:58:42 +0900

    Modify remove unused Screen in MainStack
    Former-commit-id: 5acdb428847260f56085e8fc0bce4df1fafddfad

--- frontend/src/navigations/MainStack.tsx
+++ frontend/src/navigations/MainStack.tsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import { ThemeContext } from 'styled-components/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { Header, DateModal } from '../components';
+import { Header } from '../components';
 import { View, StyleSheet } from 'react-native';
 import BottomTab from './BottomTab';
 import {
@@ -50,7 +50,6 @@ const MainStack = () => {
         <Stack.Screen name="RecipeDetail" component={RecipeDetailScreen} />
         <Stack.Screen name="Tab" component={BottomTab} />
         <Stack.Screen name="OCR" component={OCRScreen} />
-        <Stack.Screen name="Scan" component={DateModal} />
       </Stack.Navigator>
     </View>
   );
