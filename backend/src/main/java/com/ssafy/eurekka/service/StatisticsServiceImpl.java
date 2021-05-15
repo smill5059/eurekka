0c100d11626c24bfb9d5fb78d531cf111a868782
송현아 <sohyua2@naver.com>
2021-05-15 13:28:19 +0900

    Modify statistics
    -
    backend/src/main/java/com/ssafy/eurekka/service/StatisticsServiceImpl.java
    : indexOutOfBounds 에러 수정
    
    
    Former-commit-id: fea0704b972cf8d52e33449815a796e6bde91122

--- backend/src/main/java/com/ssafy/eurekka/service/StatisticsServiceImpl.java
+++ backend/src/main/java/com/ssafy/eurekka/service/StatisticsServiceImpl.java
@@ -7,7 +7,11 @@ import com.ssafy.eurekka.vo.DoneProduct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 
 @Service
 public class StatisticsServiceImpl implements StatisticsService{
