79fc038be01bc1140e0ea89ca84eb119b40d4489
junwoong <ajx0114@naver.com>
2021-05-16 17:08:37 +0900

    Modify recipe.py
    -  flask/recipe.py : 변수 정리 및 시간 수정
    
    
    Former-commit-id: 1f455723e454265af9b2b1022173b0aee239aa5e

--- flask/recipe.py
+++ flask/recipe.py
@@ -1,14 +1,10 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-import time
-import json
 from apscheduler.schedulers.background import BackgroundScheduler
-from apscheduler.triggers.interval import IntervalTrigger
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
 import pandas as pd
-import numpy as np
 import warnings
 import datetime
 from pyfcm import FCMNotification
@@ -29,31 +25,18 @@ app.config['JSON_AS_ASCII'] = False
 mongo = PyMongo(app)
 
 APIKEY = "AAAAbJys5pQ:APA91bFkH7uAoWrakN-xJyNIZpOSnA-PA1hUONVyOvEOXJ1mRV_KGT10t8ti1VGnt_bTYkS2FBdAuSAnYIZgfSmL0hbbbQ3GR2ALD2YfGVIJGcSHz7Zl7U0sZ1HOC03kFlc4r2BlPUkK"
-# TOKEN = "f0yNGiATSASazBLNsYenTW:APA91bHgkMWiXYFaIEnA5W-E7pXHZ8ocp-8HV2Xn66UObqvFi-hcaqafPbU5pNjOTd8SFHZmC1epC_1P1zbJxdDviui8cWLH-vF42-6ENrCdg_aAjij4IgqY8TzrhUZ6pUmyYxEsLoKb"
- 
 # 파이어베이스 콘솔에서 얻어 온 서버 키를 넣어 줌
-push_service = FCMNotification(APIKEY)
+pushService = FCMNotification(APIKEY)
 
 @app.route('/') # to check whether the application is running correctly on port
-def hello_flask():
+def checkFlask():
     return 'flask is running correctly'
 
-# @app.route('/viewRecipe', methods=['GET']) #viewing all contents of recipe
-# def get_recipe():
-#    recipe = mongo.db.recipe
-#    dishes = []
-#    dish = recipe.find()
-#    for r in dish:
-#       r.pop('_id')
-#       dishes.append(r)
-#    return jsonify(dishes)
-
-@app.route('/viewRecipe/<refrigeratorId>', methods=['GET'])
-def get_recipe(refrigeratorId):
-
+@app.route('/recipe/<refrigerId>', methods=['GET'])
+def getRecipe(refrigerId):
    # 해당 냉장고 아이디의 재료를 다 가져온다.
    refrigerator = mongo.db.refrigerator
-   UserRefrigerator = refrigerator.find_one({'_id' : ObjectId(refrigeratorId)})
+   UserRefrigerator = refrigerator.find_one({'_id' : ObjectId(refrigerId)})
    products = []
    noodles = UserRefrigerator['noodles']
    for product in noodles:
@@ -124,28 +107,28 @@ def get_recipe(refrigeratorId):
    dishes = []
    dish = recipe.find()
    
-   for r in dish:
-      r.pop('_id') # r dict 
-      dishes.append(r)
+   for d in dish:
+      d.pop('_id') # d dict 
+      dishes.append(d)
 
    recipes = pd.DataFrame(dishes)
    recipes = recipes.append({'seq' : '1313' , 'section' : 'refrigerator', 'title' : '내 냉장고', 'parts' : ingredients} , ignore_index=True)
-   count_vect = CountVectorizer(min_df=0, ngram_range=(1,2))
-   prefer_mat = count_vect.fit_transform(recipes['parts'])
-   prefer_sim = cosine_similarity(prefer_mat, prefer_mat)
-   similar_percentage = pd.DataFrame(prefer_sim)
-   recipes['percentage']=similar_percentage[1312]
-   prefer_sim_sorted_ind = prefer_sim.argsort()[:, ::-1]
-   similar_recipes = find_sim_recipes(recipes, prefer_sim_sorted_ind, '내 냉장고')
-   similar_recipes = similar_recipes[similar_recipes['section'] == 'food'][:20]
-   similar_recipes = similar_recipes.reset_index(drop=True)
-   similar_recipes_dict = similar_recipes.to_dict(orient = 'index')
-   return jsonify(similar_recipes_dict)
+   countVect = CountVectorizer(min_df=0, ngram_range=(1,2))
+   ingredientMat = countVect.fit_transform(recipes['parts'])
+   ingredientSim = cosine_similarity(ingredientMat, ingredientMat)
+   similarPercentage = pd.DataFrame(ingredientSim)
+   recipes['percentage'] = similarPercentage[1312]
+   ingredientSimSortedIndex = ingredientSim.argsort()[:, ::-1]
+   similarRecipes = findSimRecipes(recipes, ingredientSimSortedIndex, '내 냉장고')
+   similarRecipes = similarRecipes[similarRecipes['section'] == 'food'][:20]
+   similarRecipes = similarRecipes.reset_index(drop=True)
+   similarRecipesDict = similarRecipes.to_dict(orient = 'index')
+   return jsonify(similarRecipesDict)
 
-@app.route('/checkAlarm/<refrigeratorId>', methods=['GET'])
-def check_alarm(refrigeratorId):
+@app.route('/recipe/checkAlarm/<refrigerId>', methods=['GET'])
+def checkAlarm(refrigerId):
    refrigerator = mongo.db.refrigerator
-   UserRefrigerator = refrigerator.find_one({'_id' : ObjectId(refrigeratorId)})
+   UserRefrigerator = refrigerator.find_one({'_id' : ObjectId(refrigerId)})
    products = []
    noodles = UserRefrigerator['noodles']
    for product in noodles:
@@ -211,8 +194,8 @@ def check_alarm(refrigeratorId):
    check = False
    for product in products:   
       now = datetime.datetime.now()
-      effective_days = (product['expirationDate'] - now).days
-      if effective_days < 3 :
+      effectiveDays = (product['expirationDate'] - now).days
+      if effectiveDays < 3 :
          check = True
    
    if check == True :
@@ -221,10 +204,10 @@ def check_alarm(refrigeratorId):
       return "False"
 
 
-@app.route('/alarmList/<refrigeratorId>', methods=['GET'])
-def get_alarmList(refrigeratorId):
+@app.route('/recipe/alarmList/<refrigerId>', methods=['GET'])
+def getAlarmList(refrigerId):
    refrigerator = mongo.db.refrigerator
-   UserRefrigerator = refrigerator.find_one({'_id' : ObjectId(refrigeratorId)})
+   UserRefrigerator = refrigerator.find_one({'_id' : ObjectId(refrigerId)})
    products = []
    noodles = UserRefrigerator['noodles']
    for product in noodles:
@@ -287,25 +270,25 @@ def get_alarmList(refrigeratorId):
       product.pop('_id')
       products.append(product)
 
-   necessary_food =pd.DataFrame()
+   necessaryAlarmIngredient =pd.DataFrame()
    check = False
    for product in products:   
       now = datetime.datetime.now()
-      effective_days = (product['expirationDate'] - now).days
-      if effective_days < 3 :
-         necessary_food = necessary_food.append({'name' : product['name'],'ingredient' : product['ingredient'],'dday' : effective_days},ignore_index=True)
+      effectiveDays = (product['expirationDate'] - now).days
+      if effectiveDays < 3 :
+         necessaryAlarmIngredient = necessaryAlarmIngredient.append({'name' : product['name'],'ingredient' : product['ingredient'],'dday' : effectiveDays},ignore_index=True)
          check = True
    
    if check == True :
-      necessary_food = necessary_food.to_dict(orient = 'index')
-      return jsonify(necessary_food)
+      necessaryAlarmIngredient = necessaryAlarmIngredient.to_dict(orient = 'index')
+      return jsonify(necessaryAlarmIngredient)
    else : 
       return "정보없음"
 
-@app.route('/alarmRecipe/<refrigeratorId>&<ingredient>', methods=['GET'])
-def get_alarmRecipe(refrigeratorId,ingredient):
+@app.route('/recipe/alarmRecipe/<refrigerId>&<ingredient>', methods=['GET'])
+def getAlarmRecipe(refrigerId,ingredient):
    refrigerator = mongo.db.refrigerator
-   UserRefrigerator = refrigerator.find_one({'_id' : ObjectId(refrigeratorId)})
+   UserRefrigerator = refrigerator.find_one({'_id' : ObjectId(refrigerId)})
    products = []
    noodles = UserRefrigerator['noodles']
    for product in noodles:
@@ -377,29 +360,29 @@ def get_alarmRecipe(refrigeratorId,ingredient):
    dishes = []
    dish = recipe.find()
    
-   for r in dish:
-      r.pop('_id') # r dict 
-      dishes.append(r)
+   for d in dish:
+      d.pop('_id') # d dict 
+      dishes.append(d)
 
    recipes = pd.DataFrame(dishes)
    recipes = recipes.append({'seq' : '1313' , 'section' : 'refrigerator', 'title' : '내 냉장고', 'parts' : ingredients} , ignore_index=True)
-   count_vect = CountVectorizer(min_df=0, ngram_range=(1,2))
-   prefer_mat = count_vect.fit_transform(recipes['parts'])
-   prefer_sim = cosine_similarity(prefer_mat, prefer_mat)
-   similar_percentage = pd.DataFrame(prefer_sim)
-   recipes['percentage']=similar_percentage[1312]
-   prefer_sim_sorted_ind = prefer_sim.argsort()[:, ::-1]
-   similar_recipes = find_sim_recipes(recipes, prefer_sim_sorted_ind, '내 냉장고')
-   similar_recipes = similar_recipes[similar_recipes['section'] == 'food']
-   similar_recipes = similar_recipes[similar_recipes['parts'].str.contains(ingredient)]
-   size = similar_recipes['section'].count()
+   countVect = CountVectorizer(min_df=0, ngram_range=(1,2))
+   ingredientMat = countVect.fit_transform(recipes['parts'])
+   ingredientSim = cosine_similarity(ingredientMat, ingredientMat)
+   similarPercentage = pd.DataFrame(ingredientSim)
+   recipes['percentage']=similarPercentage[1312]
+   ingredientSimSortedIndex = ingredientSim.argsort()[:, ::-1]
+   similarRecipes = findSimRecipes(recipes, ingredientSimSortedIndex, '내 냉장고')
+   similarRecipes = similarRecipes[similarRecipes['section'] == 'food']
+   similarRecipes = similarRecipes[similarRecipes['parts'].str.contains(ingredient)]
+   size = similarRecipes['section'].count()
    if size < 6 : 
-      no_recipe = recipes[recipes['section'] == 'no_recipe']
-      similar_recipes = pd.concat([similar_recipes,no_recipe]) 
-   similar_recipes = similar_recipes.reset_index(drop=True)
-   similar_recipes = similar_recipes[:5]
-   similar_recipes_dict = similar_recipes.to_dict(orient = 'index')
-   return jsonify(similar_recipes_dict)
+      noRecipe = recipes[recipes['section'] == 'no_recipe']
+      similarRecipes = pd.concat([similarRecipes,noRecipe]) 
+   similarRecipes = similarRecipes.reset_index(drop=True)
+   similarRecipes = similarRecipes[:5]
+   similarRecipesDict = similarRecipes.to_dict(orient = 'index')
+   return jsonify(similarRecipesDict)
 
 
 def pushRecipe():
@@ -407,10 +390,10 @@ def pushRecipe():
    refrigerator = mongo.db.refrigerator
    
    for user in users :
-      refrigeratorId = user['refrigeratorId']
+      refrigerId = user['refrigeratorId']
       userToken = user['deviceToken']
       # 해당 냉장고 아이디의 재료를 다 가져온다.
-      UserRefrigerator = refrigerator.find_one({'_id' : ObjectId(refrigeratorId)})
+      UserRefrigerator = refrigerator.find_one({'_id' : ObjectId(refrigerId)})
       products = []
       noodles = UserRefrigerator['noodles']
       for product in noodles:
@@ -473,58 +456,44 @@ def pushRecipe():
          product.pop('_id')
          products.append(product)
 
-      necessary_food =[]
+      necessaryAlarmIngredient =[]
       check = False
       for product in products:   
          now = datetime.datetime.now()
-         effective_days = (product['expirationDate'] - now).days
-         if effective_days < 3 :
-            necessary_food.append(product['name'])
+         effectiveDays = (product['expirationDate'] - now).days
+         if effectiveDays < 3 :
+            necessaryAlarmIngredient.append(product['name'])
             check = True
       
       if check == True :
-         for food in necessary_food : 
+         for food in necessaryAlarmIngredient : 
             sendMessage("유레까", food + " 지금 유통기한이 얼마 남지 않았습니다!!", userToken)
 
-
-
-def find_sim_recipes(df, sorted_ind, my_refrigerator):
-    
-    # 인자로 입력된 plants DataFrame에서 'RCP_NM' 컬럼이 입력된 my_refrigerator 값인 DataFrame추출
-    refrigerator = df[df['title'] == my_refrigerator]
-    
+def findSimRecipes(df, sortedIndex, myRefrigerator):
+    # 인자로 입력된 plants DataFrame에서 'RCP_NM' 컬럼이 입력된 myRefrigerator 값인 DataFrame추출
+    refrigerator = df[df['title'] == myRefrigerator]
     # my_refrigerator을 가진 DataFrame의 index 객체를 ndarray로 반환하고 
-    # sorted_ind 인자로 입력된 prefer_sim_sorted_ind 객체에서 유사도 순으로 top_n 개의 index 추출
+    # sortedIndex 인자로 입력된 ingredientSimSortedIndex 객체에서 유사도 순으로 top_n 개의 index 추출
     refrigerator_index = refrigerator.index.values
-    
-    similar_indexes = sorted_ind[refrigerator_index]
-    
+    similarIndexes = sortedIndex[refrigerator_index]
     # 추출된 top_n index들 출력. top_n index는 2차원 데이터 임. 
     #dataframe에서 index로 사용하기 위해서 1차원 array로 변경
+    similarIndexes = similarIndexes.reshape(-1)
+    return df.iloc[similarIndexes]
 
-    similar_indexes = similar_indexes.reshape(-1)
-    
-    return df.iloc[similar_indexes]
-
-scheduler = BackgroundScheduler()
-intervalTrigger = IntervalTrigger(seconds=60)
-scheduler.add_job(pushRecipe, intervalTrigger, id='pushRecipe_id')
-scheduler.start()
-
-
- 
 def sendMessage(body, title, userToken):
     # 메시지 (data 타입)
-    data_message = {
+    dataMessage = {
         "body": body,
         "title": title
     }
     # 토큰값을 이용해 1명에게 푸시알림을 전송함
-    result = push_service.single_device_data_message(registration_id=userToken, data_message=data_message)
-    # 전송 결과 출력
+    result = pushService.single_device_data_message(registration_id=userToken, data_message=dataMessage)
     print(result)
- 
-# sendMessage("유레까", "우유 지금 유통기한이 얼마 남지 않았습니다!!")
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(pushRecipe, 'cron', hour=17, id='pushRecipe_id')
+scheduler.start()
 
 if __name__ == "__main__":
     app.debug = False
