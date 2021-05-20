6f8b33baaec3453e5598d125f3d8de46f583dd89
박서연 <psy3153@naver.com>
2021-05-20 18:30:23 +0900

    Modify eurekkaHomePage
    - eurekka_homepage/src/App.vue : media.css 등록
    - eurekka_homepage/src/assets/css/home.css : css 수정
    - eurekka_homepage/src/assets/css/media.css : 반응형 웹 제작
    - eurekka_homepage/src/assets/image/download.png : 다운로드 이미지
    - eurekka_homepage/src/components/Main/Banner.vue : css 수정
    - eurekka_homepage/src/components/Main/Recipe.vue : css 수정
    - eurekka_homepage/src/router/index.js : router 추가
    - eurekka_homepage/src/views/Home.vue : 개인정보처리방침 등록
    - eurekka_homepage/src/views/Privacy.vue : 개인정보처리방침 페이지
    
    
    Former-commit-id: da852ac0289585b08eeccb7876f8f2e10ce3eb9d

--- eurekka_homepage/src/views/Home.vue
+++ eurekka_homepage/src/views/Home.vue
@@ -9,17 +9,17 @@
         <h3><span>유통기한부터 레시피까지</span></h3>
         <h3><span id="title">유레까 다운받으러 가기</span></h3>
       </div>
-      <a id="link" href="#" target="_blank"
+      <a
+        id="link"
+        href="https://drive.google.com/file/d/1kjAdnft2HD3FVyMIn_VR-c4N85BkQivU/view?usp=sharing"
+        target="_blank"
         ><img
-          src="https://static.wixstatic.com/media/b54765_4abf6834949a4974b5393c094fb09763~mv2.png/v1/fill/w_161,h_47,al_c,q_85,usm_0.66_1.00_0.01/Icons%2520App%2520Store%2520Google%2520play_edit.webp"
+          src="@/assets/image/download.png"
           alt="Icons%20App%20Store%20Google%20play_edit"
-          style="
-            width: 161px;
-            height: 47px;
-            object-fit: cover;
-            object-position: 50% 50%;
-          "
       /></a>
+      <div id="footer">
+        <button @click="movePrivacy()">개인정보처리방침</button>
+      </div>
     </div>
   </div>
 </template>
@@ -37,5 +37,28 @@ export default {
     Recipe,
     Graph,
   },
+  methods: {
+    movePrivacy() {
+      document.documentElement.scrollTop = 0;
+      this.$router.push({ name: 'Privacy' }).catch((error) => {
+        if (error.name === 'NavigationDuplicated') {
+          location.reload();
+        }
+      });
+    },
+  },
 };
 </script>
+<style>
+#footer {
+  padding-bottom: 1%;
+}
+#footer button {
+  cursor: pointer;
+  font-size: 14px;
+  font-family: 'ChosunSg';
+  font-weight: bold;
+  border: none;
+  background-color: #e4e9f0;
+}
+</style>
